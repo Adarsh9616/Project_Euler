008a7fdca04cbf58ebf14c077d54b5421391eea5
Adarsh Verma <34129174+Adarsh9616@users.noreply.github.com>
2020-05-23 02:11:04 +0530

    Add files via upload

---
+++ Problem5.java
@@ -0,0 +1,25 @@
+public class Problem5
+{
+    static long gcd(long a,long b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    static long lcm(long a,long b)
+    {
+        return (a*b)/gcd(a,b);
+    }
+    public static void main(String args[])
+    {
+        long ans=1;
+        for(int i=1;i<=20;i++)
+        {
+            ans=lcm(ans,i);
+        }
+        System.out.println(ans);
+
+    }
+}
